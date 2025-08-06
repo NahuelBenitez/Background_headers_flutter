18f4edeeaee25a096dbac913b87a3ba359c6fd3e
NahuelBenitez <jorgenahuelbenitez@gmail.com>
2025-08-06 16:17:31 -0300

    header cuadrado

---
+++ lib/src/pages/headers_page.dart
@@ -0,0 +1,15 @@
+import 'package:backgrounds_styles/src/pages/widgets/headers.dart';
+import 'package:flutter/material.dart';
+class HeadersPage extends StatelessWidget {
+  const HeadersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HeaderCuadrado()
+      // Center(
+      //   child: Text("hola mundo"),
+      // ),
+    );
+  }
+}
---
+++ lib/src/pages/widgets/headers.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HeaderCuadrado extends StatelessWidget {
+  const HeaderCuadrado({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 500,
+      color: Colors.blueAccent,
+    );
+  }
+}
